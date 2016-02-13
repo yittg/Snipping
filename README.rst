48285483fe16eb78b1bacab1939e4e774135abcb
Yi Tang <ssnailtang@gmail.com>
2016-02-13 17:02:50 +0800

    Convert markdown readme to reStructuredText

---
+++ README.rst
@@ -0,0 +1,28 @@
+|Build Status|
+
+Snipping
+========
+
+Edit and run **python** snippet in real time.
+
+Usage
+=====
+
+::
+
+    $ snipping
+
+Special thanks
+==============
+
+-  `prompt\_toolkit <http://github.com/jonathanslenders/python-prompt-toolkit>`__
+
+Notice
+======
+
+In ``snipping``, ``sys.stdout`` and ``sys.stderr`` is redirected to a
+``stringIO``, so actions related to them may be different from the
+original Python.
+
+.. |Build Status| image:: https://travis-ci.org/yittg/Snipping.svg?branch=master
+   :target: https://travis-ci.org/yittg/Snipping
--- setup.py
+++ setup.py
@@ -1,11 +1,8 @@
-import os
-
 from setuptools import setup, find_packages
 
 
 def readme():
-    readme_file = os.path.join(os.path.dirname(__file__), 'README.md')
-    with open(readme_file, 'r') as rf:
+    with open("README.rst", 'r') as rf:
         return rf.read()
 
 setup(
@@ -13,10 +10,10 @@ setup(
     author='Yi Tang',
     author_email='ssnailtang@gmail.com',
     version='0.1',
-    description='Edit and run snippet in realtime',
+    description='Edit and run snippet in real time',
     long_description=readme(),
     url='https://github.com/yittg/snipping',
-    licence='MIT',
+    license='MIT',
     packages=find_packages('.'),
     install_requires=[
         'prompt_toolkit',
@@ -27,5 +24,4 @@ setup(
         'console_scripts': [
             'snipping = snipping.main:main',
         ],
-    }
-)
+    })
