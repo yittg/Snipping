a1923898e0195653b8923b90d5a1660c0cc68ea4
Yi Tang <ssnailtang@gmail.com>
2016-02-15 21:14:53 +0800

    Set text window min size only

--- snipping/layout.py
+++ snipping/layout.py
@@ -6,18 +6,21 @@ from snipping.prompt_toolkit import style
 HEADER = 'Snipping'
 FOOTER = 'Quit: ctrl-c  Save: <F4>'
 
+MAIN_WINDOW_SIZE = 100
+SUB_WINDOW_SIZE = 40
+
 
 def create_layout(contents):
     result_windows = []
     for content in contents:
         if result_windows:
-            result_windows.append(layout.horizontal_line(40, 80))
+            result_windows.append(layout.horizontal_line(SUB_WINDOW_SIZE))
         result_windows.append(layout.normal_text_window(
-            name=content, width=layout.dim(40, 80))
+            name=content, width=layout.dim(SUB_WINDOW_SIZE))
         )
     result_layout = layout.window_rows(result_windows)
-    editor_window = layout.normal_text_window(lineno=True,
-                                              trailing_space=True)
+    editor_window = layout.normal_text_window(
+        lineno=True, trailing_space=True, width=layout.dim(MAIN_WINDOW_SIZE))
     main_layout = layout.window_columns([
         editor_window, layout.vertical_line(), result_layout
     ])
